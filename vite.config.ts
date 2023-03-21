cbbf3143e0b0d0e45e2729cb763cac1b14613016
Aaron <wo_99936@qq.com>
2023-03-21 19:10:58 +0800

    feat(add): "添加部署"

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,12 @@ import { createBuild } from "./build/vite/build";
 import { createServer } from "./build/vite/server";
 
 export default (env: ConfigEnv) => {
+
+  const isProd = env.mode !== "development";
+  const base = isProd ? "/grow_up_admin/" : "/";
+
   return {
+    base,
     plugins: createPlugin(env),
     css: createCss(),
     resolve: createResolve(),
