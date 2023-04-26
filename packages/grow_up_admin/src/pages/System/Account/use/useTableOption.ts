742c79503c6c758e5c3a92a5e1f4fd8a321a7aa3
Aaron <827265905@qq.com>
2023-04-26 13:59:28 +0800

    feat(modify): "还原代码"

--- packages/grow_up_admin/src/pages/System/Account/use/useTableOption.ts
+++ packages/grow_up_admin/src/pages/System/Account/use/useTableOption.ts
@@ -16,10 +16,104 @@ export const useTableOption = ({ tableTotal, accountStates }: props) => {
   const state = reactive({
     tableList: []
   });
-  const tableCols = ref([]);
 
   // ~ 表头配置
-  const tableColumns = computed(() => tableCols.value);
+  const tableColumns = computed(() => [
+    {
+      field: "serial",
+      title: "序号",
+      fixed: "left"
+    },
+    {
+      field: "account",
+      title: "账号",
+      "show-overflow-tooltip": true,
+      width: 120
+    },
+    {
+      field: "state",
+      title: "状态",
+      "show-overflow-tooltip": true,
+      width: 120,
+      formatter: (space: any): any[] => {
+        const item = accountStates.value.find((el) => el.code === space.state);
+        const type = ["danger", "success"][item.code] as "success" | "danger";
+        return [
+          h(
+            ElTag,
+            {
+              type
+            },
+            item.label
+          )
+        ];
+      }
+    },
+    {
+      field: "forbidCause",
+      title: "停用原因",
+      "show-overflow-tooltip": true,
+      width: 100,
+      visible: false
+    },
+    {
+      field: "roleName",
+      title: "角色名称",
+      "show-overflow-tooltip": true
+    },
+    {
+      field: "belong",
+      title: "归属",
+      "show-overflow-tooltip": true,
+      visible: false,
+      children: [
+        {
+          field: "belong.person",
+          title: "归属人",
+          "show-overflow-tooltip": true,
+          width: 150,
+          visible: false
+        },
+        {
+          field: "belong.department",
+          title: "所属部门",
+          "show-overflow-tooltip": true,
+          width: 150,
+          visible: false
+        },
+        {
+          field: "belong.post",
+          title: "所属岗位",
+          "show-overflow-tooltip": true,
+          width: 150,
+          visible: false
+        }
+      ]
+    },
+    {
+      field: "createType",
+      title: "创建方式",
+      "show-overflow-tooltip": true,
+      width: 100
+    },
+    {
+      field: "createDate",
+      title: "创建日期",
+      "show-overflow-tooltip": true
+    },
+    {
+      field: "remarks",
+      title: "备注",
+      "show-overflow-tooltip": true,
+      width: 140
+    },
+    {
+      field: "operate",
+      title: "操作",
+      fixed: "right",
+      width: "200px"
+    }
+  ]);
 
   const tableList = computed(() => {
     return state.tableList;
@@ -39,104 +133,6 @@ export const useTableOption = ({ tableTotal, accountStates }: props) => {
 
   onMounted(async () => {
     await getAccountList();
-    setTimeout(() => {
-      tableCols.value = [
-        {
-          field: "serial",
-          title: "序号",
-          fixed: "left"
-        },
-        {
-          field: "account",
-          title: "账号",
-          "show-overflow-tooltip": true,
-          width: 120
-        },
-        {
-          field: "state",
-          title: "状态",
-          "show-overflow-tooltip": true,
-          width: 120,
-          formatter: (space: any): any[] => {
-            const item = accountStates.value.find((el) => el.code === space.state);
-            const type = ["danger", "success"][item.code] as "success" | "danger";
-            return [
-              h(
-                ElTag,
-                {
-                  type
-                },
-                item.label
-              )
-            ];
-          }
-        },
-        {
-          field: "forbidCause",
-          title: "停用原因",
-          "show-overflow-tooltip": true,
-          width: 100,
-          visible: false
-        },
-        {
-          field: "roleName",
-          title: "角色名称",
-          "show-overflow-tooltip": true
-        },
-        {
-          field: "belong",
-          title: "归属",
-          "show-overflow-tooltip": true,
-          visible: false,
-          children: [
-            {
-              field: "belong.person",
-              title: "归属人",
-              "show-overflow-tooltip": true,
-              width: 150,
-              visible: false
-            },
-            {
-              field: "belong.department",
-              title: "所属部门",
-              "show-overflow-tooltip": true,
-              width: 150,
-              visible: false
-            },
-            {
-              field: "belong.post",
-              title: "所属岗位",
-              "show-overflow-tooltip": true,
-              width: 150,
-              visible: false
-            }
-          ]
-        },
-        {
-          field: "createType",
-          title: "创建方式",
-          "show-overflow-tooltip": true,
-          width: 100
-        },
-        {
-          field: "createDate",
-          title: "创建日期",
-          "show-overflow-tooltip": true
-        },
-        {
-          field: "remarks",
-          title: "备注",
-          "show-overflow-tooltip": true,
-          width: 140
-        },
-        {
-          field: "operate",
-          title: "操作",
-          fixed: "right",
-          width: "200px"
-        }
-      ]
-    }, 2000);
   });
 
   return {
