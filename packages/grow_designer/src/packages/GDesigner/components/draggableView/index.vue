86ae659c966459dd1660c4d88f931faca07ae87a
张鱼烧。 <827265905@qq.com>
2023-07-07 16:55:05 +0800

    feat(add): "添加页面设计器交互"

--- packages/grow_designer/src/packages/GDesigner/components/draggableView/index.vue
+++ packages/grow_designer/src/packages/GDesigner/components/draggableView/index.vue
@@ -1,22 +1,35 @@
 <template>
     <VueDraggableNext group="draggable-group"
                       :animation="180"
-                      v-model="draggableList"
-                      class="h-full bg-BG_COLOR2">
-      <transition-group>
+                      :modelValue="draggableList"
+                      class="draggable-grop-wrap h-full bg-BG_COLOR2">
+      <transition-group type="transition">
         <div v-for="ele in draggableList"
-             :key="ele.name + Math.random()">{{ ele.name }}</div>
+             :key="ele.elName + Math.random()">{{ ele.elName }}</div>
       </transition-group>
     </VueDraggableNext>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
 import { VueDraggableNext } from "vue-draggable-next";
 
-const draggableList = ref([]);
+interface Props {
+  draggableList: any[]
+};
+
+const props = defineProps<Props>();
 
 defineOptions({ name: "draggableView" })
 
 </script>
 
+<style lang="scss">
+.draggable-grop-wrap {
+  .draggable-item {
+    width: 100% !important;
+    display: block !important;
+    height: auto !important;
+    transition: all .35s !important;
+  }
+}
+</style>
--- packages/grow_designer/src/packages/GDesigner/static/moduleMap.ts
+++ packages/grow_designer/src/packages/GDesigner/static/moduleMap.ts
@@ -1,12 +1,56 @@
 
-export const BaseComponent = {
+export const BaseComponent = new Map([
+  ["basicsElement", {
+    title: "基础元素",
+    group:[{
+      elName: "图片",
+      elType: "basic",
+      elTagName: "img",
+      elIcon: "Image"
+    },{
+      elName: "标题",
+      elType: "basic",
+      elTagName: "h5",
+      elIcon: "FontColorsOutlined"
+    },{
+      elName: "正文",
+      elType: "basic",
+      elTagName: "p",
+      elIcon: "FontSizeOutlined"
+    },{
+      elName: "短语",
+      elType: "basic",
+      elTagName: "span",
+      elIcon: "ItalicOutlined"
+    }]
+  }],
+  ["layoutElement", {
+    title: "布局容器",
+    group: [{
+      elName: "卡片",
+      elType: "eleModule",
+      elTagName: "el-card",
+      elIcon: "IdcardOutlined"
+    },{
+      elName: "选项卡",
+      elType: "eleModule",
+      elTagName: "el-tabs",
+      elIcon: "DataBaseAlt"
+    },{
+      elName: "布局",
+      elType: "eleModule",
+      elTagName: "el-row",
+      elIcon: "OpenPanelRight"
+    },{
+      elName: "分割线",
+      elType: "eleModule",
+      elTagName: "el-divider",
+      elIcon: "BorderVerticleOutlined"
+    }]
+  }]
+]);
 
-};
 
-export const BusinessComponent = {
+export const BusinessComponent = new Map();
 
-};
-
-export const AtomicComponent = {
-
-};
+export const AtomicComponent = new Map();
