cbe8e17799fb0d13af2e7cbcc7861c1c8c09e415
张鱼烧。 <827265905@qq.com>
2023-08-16 11:26:15 +0800

    feat(add): "添加页面设计器交互"

---
+++ packages/grow_designer/src/packages/GDesigner/components/abstractionComponent/component/basicComponent/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <component :is="config.elTagName">111</component>
+</template>
+
+<script setup lang="ts">
+
+interface PropsType {
+  config: any;
+}
+
+const props = withDefaults(defineProps<PropsType>(), {
+  config: () => ({})
+});
+
+
+</script>
