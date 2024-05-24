75660785972a885214bebc9133a689c23a42f6bf
张鱼烧。 <827265905@qq.com>
2024-05-24 14:29:01 +0800

    feat(add): "增加简单在线编辑器，后续需要持续优化增加功能"

--- packages/grow_editor/src/packages/Codemirror/use/useEvent.ts
+++ packages/grow_editor/src/packages/Codemirror/use/useEvent.ts
@@ -1,14 +1,16 @@
 
-
 export const useEvent = ({
     editorView,
     codeEditor,
-    resetEditor, editorValue
+    resetEditor,
+    editorValue,
+    emit
 }) => {
 
     const onModelChange = (value) => {
         codeEditor.value.innerHTML = "";
         resetEditor(codeEditor.value, value.method);
+        emit("lang-change", value);
     };
 
     const onCleanEditor= () => {
@@ -16,8 +18,20 @@ export const useEvent = ({
         resetEditor(codeEditor.value, editorValue.value.method);
     }
 
+    const toJSON = () => {
+        return editorView.value.state.toJSON();
+    };
+
+    const setDoc = (doc, isRunChange = false) => {
+        codeEditor.value.innerHTML = "";
+        resetEditor(codeEditor.value, editorValue.value.method, doc);
+        isRunChange && emit("change", editorView.value.state.toJSON());
+    }
+
     return {
         onCleanEditor,
-        onModelChange
+        onModelChange,
+        toJSON,
+        setDoc
     }
 }
--- packages/grow_up_admin/src/pages/AutomatedTool/OnlineEditor/index.vue
+++ packages/grow_up_admin/src/pages/AutomatedTool/OnlineEditor/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <GCodemirror />
+  <GCodemirror ref="codemirrorRef"/>
 </template>
 
 <script setup>
 defineOptions({ name: "OnlineEditor" });
 import { GCodemirror } from "grow_editor";
+import { ref, onMounted } from "vue";
+const codemirrorRef = ref(null);
 
+onMounted(() => {
+  codemirrorRef.value.setDoc(`//  这里是注释\n/*这里也是注释*/\nvar a = 1;\nvar b = 2;\nvar c = 3;\nvar d = c - b - a;`);
+});
 </script>
