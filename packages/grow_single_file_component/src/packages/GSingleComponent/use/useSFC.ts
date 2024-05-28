e918c411214669ed68b278ab56cf967ad1d94903
张鱼烧。 <827265905@qq.com>
2024-05-28 17:37:27 +0800

    feat(add): "修复生产环境不显示问题"

--- packages/grow_single_file_component/src/packages/GSingleComponent/use/useSFC.ts
+++ packages/grow_single_file_component/src/packages/GSingleComponent/use/useSFC.ts
@@ -1,12 +1,15 @@
 import { reactive } from "vue";
+import * as ELEMENT from "element-plus";
 
 export const useSFC = () => {
     let options = reactive({
         files: {
-            "App.vue": "<template>\n  <CeShi /> \n  <ElButton @click=\"onClick\">Num Add = {{ num }}</ElButton>\n</template>\n\n<script setup>\nimport CeShi from \"./CeShi.vue\";\nimport { ref } from \"vue\";\n\nconst num = ref(0);\n  \nconst onClick = () => {\n  console.log(\"click Me!!!\");\n  num.value += 1;\n};\n</script>\n\n<style>\n\n</style>",
+            "App.vue": "<template>\n  <CeShi /> \n  <ElButton @click=\"onClick\">Num Add = {{ num }}</ElButton>\n</template>\n\n<script setup>\nimport CeShi from \"./CeShi.vue\";\nimport { ref } from \"vue\";\nimport { ElButton } from \"element-plus\";\n\nconst num = ref(0);\n  \nconst onClick = () => {\n  console.log(\"click Me!!!\");\n  num.value += 1;\n};\n</script>\n\n<style>\n\n</style>",
             "CeShi.vue": "<template>\n\t<p class=\"per\">测试内容</p>\n</template>\n\n<script setup>\n\n</script>\n\n<style>\n.per {\n  color: pink;\n}\n</style>"
         },
-        imports: {}
+        imports: {
+            "element-plus": ELEMENT
+        }
     });
 
     return {
