9d770c21e6bbb80620f0e27caaaaf48b1bead99f
张鱼烧。 <827265905@qq.com>
2024-11-08 16:04:00 +0800

    feat(feat): "完善锁屏功能"

--- packages/grow_designer/src/packages/GDesigner/optionComponent/ElementSize/index.vue
+++ packages/grow_designer/src/packages/GDesigner/optionComponent/ElementSize/index.vue
@@ -2,10 +2,13 @@
   <div class="px-[10px] py-[5px]">
     <div class="relative w-full h-[150px]">
       <div class="py-[60px] px-[60px]">
-        <ElSelect :modelValue="`px`" class="w-full" size="small">
-          <ElOption label="px" name="px"></ElOption>
-          <ElOption label="%" name="%"></ElOption>
-          <ElOption label="vw" name="vw"></ElOption>
+        <ElSelect v-model="marginUnit"
+                  class="w-full"
+                  size="small"
+                  @change="onMarginUnitChange">
+          <ElOption label="px" value="px"></ElOption>
+          <ElOption label="%" value="%"></ElOption>
+          <ElOption label="vw/vh" value="vw/vh"></ElOption>
         </ElSelect>
       </div>
       <div class="absolute top-0 left-0 right-0 border-t-[20px] border-r-[20px] border-l-[20px] border-t-[#d6e4ff] border-solid border-l-[transparent] border-r-[transparent]">
@@ -14,8 +17,9 @@
                  maxlength="3"
                  height="100%"
                  autocomplete="off"
-                 value=""
-                 class="text-[12px] text-center h-[20px] p-[0px] leading-[20px] block w-full bg-transparent"/>
+                 :value="parseFloat(styleOption['margin-top']) || ''"
+                 class="text-[12px] text-center h-[20px] p-[0px] leading-[20px] block w-full bg-transparent"
+                 @input="(event) => onMarginChange('margin-top', event)"/>
         </span>
       </div>
       <div class="absolute top-[5px] bottom-[5px] right-0 width-[0px] border-t-[transparent] border-b-[transparent] border-r-[#d6e4ff] border-t-[20px] border-b-[20px] border-r-[20px]">
@@ -24,8 +28,9 @@
                  maxlength="3"
                  height="100%"
                  autocomplete="off"
-                 value=""
-                 class="text-[12px] absolute top-0 bottom-0 right-[0px] m-[auto] w-[20px] h-[20px] leading-[20px] text-center p-x-[8px] bg-transparent"/>
+                 :value="parseFloat(styleOption['margin-right']) || ''"
+                 class="text-[12px] absolute top-0 bottom-0 right-[0px] m-[auto] w-[20px] h-[20px] leading-[20px] text-center p-x-[8px] bg-transparent"
+                 @input="(event) => onMarginChange('margin-right', event)"/>
         </span>
       </div>
       <div class="absolute bottom-0 left-0 right-0 h-[0px] border-l-[transparent] border-r-[transparent] border-b-[#d6e4ff] border-l-[20px] border-b-[20px] border-r-[20px]">
@@ -35,8 +40,9 @@
                  maxlength="3"
                  height="100%"
                  autocomplete="off"
-                 value=""
-                 class="text-[12px] absolute left-0 right-0 bottom-[-20px] h-[20px] bg-transparent text-center"/>
+                 :value="parseFloat(styleOption['margin-bottom']) || ''"
+                 class="text-[12px] absolute left-0 right-0 bottom-[-20px] h-[20px] bg-transparent text-center"
+                 @input="(event) => onMarginChange('margin-bottom', event)"/>
         </span>
       </div>
       <div class="absolute top-[5px] bottom-[5px] left-0 w-[0px] border-t-[transparent] border-b-[transparent] border-l-[#d6e4ff] border-t-[20px] border-b-[20px] border-l-[20px]">
@@ -45,8 +51,9 @@
                  maxlength="3"
                  height="100%"
                  autocomplete="off"
-                 value=""
-                 class="text-[12px] absolute top-0 bottom-0 left-[0px] m-[auto] w-[20px] h-[20px] leading-[20px] text-center p-x-[8px] bg-transparent"/>
+                 :value="parseFloat(styleOption['margin-left']) || ''"
+                 class="text-[12px] absolute top-0 bottom-0 left-[0px] m-[auto] w-[20px] h-[20px] leading-[20px] text-center p-x-[8px] bg-transparent"
+                 @input="(event) => onMarginChange('margin-left', event)"/>
         </span>
       </div>
 
@@ -56,8 +63,9 @@
                  maxlength="3"
                  height="100%"
                  autocomplete="off"
-                 value=""
-                 class="text-[12px] text-center h-[20px] p-[0px] leading-[20px] block w-full bg-transparent"/>
+                 :value="parseFloat(styleOption['padding-top']) || ''"
+                 class="text-[12px] text-center h-[20px] p-[0px] leading-[20px] block w-full bg-transparent"
+                 @input="(event) => onMarginChange('padding-top', event)"/>
         </span>
       </div>
       <div class="absolute top-[30px] bottom-[30px] right-[25px] w-[0px] border-t-[transparent] border-b-[transparent] border-r-[#d6e4ff] border-t-[20px] border-b-[20px] border-r-[20px]">
@@ -66,8 +74,9 @@
                  maxlength="3"
                  height="100%"
                  autocomplete="off"
-                 value=""
-                 class="text-[12px] absolute top-0 bottom-0 right-[0px] m-[auto] w-[20px] h-[20px] leading-[20px] text-center p-x-[8px] bg-transparent"/>
+                 :value="parseFloat(styleOption['padding-right']) || ''"
+                 class="text-[12px] absolute top-0 bottom-0 right-[0px] m-[auto] w-[20px] h-[20px] leading-[20px] text-center p-x-[8px] bg-transparent"
+                 @input="(event) => onMarginChange('padding-right', event)"/>
         </span>
       </div>
       <div class="absolute bottom-[25px] left-[25px] right-[25px] h-[0px] border-l-[transparent] border-r-[transparent] border-b-[#d6e4ff] border-l-[20px] border-b-[20px] border-r-[20px]">
@@ -77,8 +86,9 @@
                  maxlength="3"
                  height="100%"
                  autocomplete="off"
-                 value=""
-                 class="text-[12px] absolute left-0 right-0 bottom-[-20px] h-[20px] bg-transparent text-center"/>
+                 :value="parseFloat(styleOption['padding-bottom']) || ''"
+                 class="text-[12px] absolute left-0 right-0 bottom-[-20px] h-[20px] bg-transparent text-center"
+                 @input="(event) => onMarginChange('padding-bottom', event)"/>
         </span>
       </div>
       <div class="absolute top-[30px] bottom-[30px] left-[25px] w-[0px] border-t-[transparent] border-b-[transparent] border-l-[#d6e4ff] border-t-[20px] border-b-[20px] border-l-[20px]">
@@ -87,27 +97,40 @@
                  maxlength="3"
                  height="100%"
                  autocomplete="off"
-                 value=""
-                 class="text-[12px] absolute top-0 bottom-0 left-[0px] m-[auto] w-[20px] h-[20px] leading-[20px] text-center p-x-[8px] bg-transparent"/>
+                 :value="parseFloat(styleOption['padding-left']) || ''"
+                 class="text-[12px] absolute top-0 bottom-0 left-[0px] m-[auto] w-[20px] h-[20px] leading-[20px] text-center p-x-[8px] bg-transparent"
+                 @input="(event) => onMarginChange('padding-left', event)"/>
         </span>
       </div>
     </div>
     <div class="flex pt-[5px]">
-      <ElInput class="flex-1 mr-[4px]" size="small" placeholder="长度" :controls="false">
+      <ElInput class="flex-1 mr-[4px]"
+               size="small"
+               placeholder="长度"
+               :controls="false"
+               :modelValue="parseFloat(styleOption['height']) || ''">
         <template #append>
           <ElSelect :modelValue="`px`" class="w-[50px]" size="small">
-            <ElOption label="px" name="px"></ElOption>
-            <ElOption label="%" name="%"></ElOption>
-            <ElOption label="vh" name="vh"></ElOption>
+            <ElOption label="px" value="px"></ElOption>
+            <ElOption label="%" value="%"></ElOption>
+            <ElOption label="vh" value="vh"></ElOption>
+            <ElOption label="auto" value="auto"></ElOption>
           </ElSelect>
         </template>
       </ElInput>
-      <ElInput class="flex-1 ml[4px]" size="small" placeholder="宽度" :controls="false">
+      <ElInput class="flex-1 ml[4px]"
+               size="small"
+               placeholder="宽度"
+               :controls="false"
+               :modelValue="parseFloat(styleOption['width']) || ''">
         <template #append>
-          <ElSelect :modelValue="`px`" class="w-[50px]" size="small">
-            <ElOption label="px" name="px"></ElOption>
-            <ElOption label="%" name="%"></ElOption>
-            <ElOption label="vw" name="vw"></ElOption>
+          <ElSelect :modelValue="`px`"
+                    class="w-[50px]"
+                    size="small">
+            <ElOption label="px" value="px"></ElOption>
+            <ElOption label="%" value="%"></ElOption>
+            <ElOption label="vw" value="vw"></ElOption>
+            <ElOption label="auto" value="auto"></ElOption>
           </ElSelect>
         </template>
       </ElInput>
@@ -116,6 +139,26 @@
 </template>
 
 <script setup lang="ts">
+import { toRefs } from "vue";
 defineOptions({ name: "ElementSize" });
+
+const emits = defineEmits(["update:styleOption"]);
+const porps = defineProps({
+  styleOption: {
+    type: Object,
+    default: () => ({})
+  }
+});
+const { styleOption } = toRefs(porps);
+import { useMargin } from "./use/useMargin";
+
+const {
+  marginUnit,
+  onMarginUnitChange,
+  onMarginChange
+} = useMargin({
+  styleOption,
+  emits
+});
 
 </script>
---
+++ packages/grow_designer/src/packages/GDesigner/optionComponent/ElementSize/use/useMargin.ts
@@ -0,0 +1,55 @@
+import { ref } from "vue";
+
+
+export const useMargin = ({
+    styleOption,
+    emits
+}) => {
+
+    const unitYMap = [
+        "margin-top",
+        "margin-bottom",
+        "padding-top",
+        "padding-bottom"
+    ];
+    const unitXMap = [
+        "margin-right",
+        "margin-left",
+        "padding-right",
+        "padding-left"
+    ]
+
+    const marginUnit = ref("px");
+
+    const onMarginUnitChange = (data: string) => {
+        const result = { ...styleOption.value };
+        for(let i = 0, item: string; item = unitYMap[i++];){
+            const value = result[item];
+            const unit = data === "vw/vh"?"vh":data;
+            value && (result[item] = `${parseFloat(value)}${unit}`);
+        }
+        for(let i =0, item: string; item = unitXMap[i++];){
+            const value = result[item];
+            const unit = data === "vw/vh"?"vw":data;
+            value && (result[item] = `${parseFloat(value)}${unit}`);
+        }
+        emits("update:styleOption", result);
+    }
+
+    const onMarginChange = (type: string, event: any) => {
+        const result = { ...styleOption.value };
+        if(event.target.value){
+            result[type] = `${event.target.value}${marginUnit.value}`;
+        }
+        else{
+            Reflect.deleteProperty(result, type);
+        }
+        emits("update:styleOption", result);
+    }
+
+    return {
+        marginUnit,
+        onMarginUnitChange,
+        onMarginChange
+    }
+};
